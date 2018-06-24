76fd31176bf097c981041eba02be566c79bb0644
masataka <ar.tec.masa@gmail.com>
2018-06-24 20:37:00 +0900

    jsファイルを追加した

---
+++ src/js/app.js
@@ -0,0 +1,20 @@
+import Vue from 'vue';
+import Router from 'vue-router';
+import Home from './components/Home.vue';
+import Detail from './components/Detail.vue';
+
+Vue.use(Router);
+
+const router = new Router({
+  mode: 'history',
+  routes: [
+    {
+      path: '/',
+      component: Home
+    },
+    {
+      path: '/blog/:slug',
+      component: Detail
+    },
+  ]
+});
